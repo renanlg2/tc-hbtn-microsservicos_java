6bd18ebada133008d460ff613aae4c9232b82ff8
Renan de Lima Gomes <renanlg@ciandt.com>
2022-07-06 08:02:09 -0300

    0. Factory Method de Personagens

---
+++ 0x00/personagem/Personagem.java
@@ -0,0 +1,55 @@
+public abstract class Personagem {
+    private String nome;
+    private TipoPersonagem tipo;
+    private int inteligencia;
+    private int forca;
+    private int vigor;
+    private int resistencia;
+    private int destreza;
+
+    public int getInteligencia() {
+        return inteligencia;
+    }
+
+    public int getForca() {
+        return forca;
+    }
+
+    public int getVigor() {
+        return vigor;
+    }
+
+    public int getResistencia() {
+        return resistencia;
+    }
+
+    public int getDestreza() {
+        return destreza;
+    }
+
+    public Personagem(String nome, TipoPersonagem tipo, int inteligencia, int forca, int vigor, int resistencia, int destreza) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.inteligencia = inteligencia;
+        this.forca = forca;
+        this.vigor = vigor;
+        this.resistencia = resistencia;
+        this.destreza = destreza;
+    }
+
+    public abstract double getDanoAtaque();
+
+    @Override
+    public String toString() {
+        return
+                "Personagem { nome = " + nome +
+                        ", tipo = " + tipo +
+                        ", inteligencia = " + inteligencia +
+                        ", forca = " + forca +
+                        ", vigor = " + vigor +
+                        ", resistencia = " + resistencia +
+                        ", destreza = " + destreza +
+                        ", dano ataque = " + String.format("%.2f", this.getDanoAtaque()) +
+                        " }";
+    }
+}
